9b2a23d9aff91b2b1c1648975818ee6b4e1bbf63
Brett Holland <brett.holland@gmail.com>
2018-04-20 18:20:58 -0400

    Updated login flow

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -1,7 +1,11 @@
 class FoldersController < ApplicationController
-	before_action :authenticate_user!
+	before_action :authenticate_user!, except: [:index]
 	def index
-		@user = current_user
+		if user_signed_in?
+			@user = current_user
+		else
+			redirect_to new_user_session_path
+		end
 	end
 	def show
 		@user = current_user
